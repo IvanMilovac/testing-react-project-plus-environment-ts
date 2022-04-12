59e8b17205698bcfa33f22f8e7a4f75eb67db334
Ivan Milovac <ivanmilovac1993@hotmail.com>
2022-04-12 22:49:57 +0200

    initial commit

---
+++ src/components/sum.spec.ts
@@ -0,0 +1,5 @@
+import sum from "./sum";
+
+it("suming 2 and 5 returns 7", () => {
+  expect(sum(5, 2)).toBe(7);
+});
---
+++ src/components/sum.ts
@@ -0,0 +1,4 @@
+export default function sum(a: number, b: number) {
+  const c = 3;
+  return c + a + b;
+}
